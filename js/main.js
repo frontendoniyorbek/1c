f003afcfa712b29122c8ff5f175afa1c97015701
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2025-05-23 17:06:18 +0500

    add navbar

--- js/main.js
+++ js/main.js
@@ -16,3 +16,33 @@ document.addEventListener('DOMContentLoaded', function () {
 		},
 	}).mount();
 });
+
+// DOM
+const menuOpen = document.querySelector('.menu__open');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileClose = document.querySelector('.menu__close');
+menuOpen.addEventListener('click', () => {
+	console.log('Salom');
+	mobileMenu.classList.add('active');
+	document.body.style.overflow = 'hidden';
+});
+
+mobileClose.addEventListener('click', () => {
+	mobileMenu.classList.remove('active');
+	document.body.style.overflow = '';
+});
+
+window.addEventListener('resize', () => {
+	if (window.innerWidth > 768) {
+		mobileMenu.classList.remove('active');
+		document.body.style.overflow = '';
+	}
+});
+
+// // Close the menu when screen is resized to larger than mobile view
+// window.addEventListener('resize', () => {
+// 	if (window.innerWidth > 820) {
+// 		mobileMenu.classList.remove('active');
+// 		document.body.style.overflow = ''; // Scrollni qayta yoqish
+// 	}
+// });
